f0a9cddacaca0b154edd27dba4307c7bed499803
nikxtaco <nikitamenon2510@gmail.com>
2022-03-27 15:33:31 +0530

    backend

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const app = require("./server");
 const server = http.createServer(app);
 
-// const { API_PORT } = process.env;
-const port = process.env.PORT || 80;
+const { API_PORT } = process.env;
+const port = process.env.PORT || API_PORT;
 
 // server listening 
 server.listen(port, () => {
